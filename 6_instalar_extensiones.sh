662a080157c6357912aa4f9fa7c917a4a7d3ca7f
William Rodriguez <wisrovi.rodriguez@gmail.com>
2022-03-30 01:29:51 -0500

    Create 6_instalar_extensiones.sh

---
+++ 6_instalar_extensiones.sh
@@ -0,0 +1,61 @@
+sudo apt-get install -y chrome-gnome-shell
+
+google-chrome https://extensions.gnome.org/extension/1401/bluetooth-quick-connect/
+
+google-chrome https://extensions.gnome.org/extension/4679/burn-my-windows/
+
+google-chrome https://extensions.gnome.org/extension/779/clipboard-indicator/
+
+google-chrome https://extensions.gnome.org/extension/3740/compiz-alike-magic-lamp-effect/
+
+google-chrome https://extensions.gnome.org/extension/97/coverflow-alt-tab/
+
+google-chrome https://extensions.gnome.org/extension/4167/custom-hot-corners-extended/
+
+google-chrome https://extensions.gnome.org/extension/307/dash-to-dock/
+
+google-chrome https://extensions.gnome.org/extension/2087/desktop-icons-ng-ding/
+
+google-chrome https://extensions.gnome.org/extension/1065/docker-status/
+
+google-chrome https://extensions.gnome.org/extension/1036/extensions/
+
+google-chrome https://extensions.gnome.org/extension/2/move-clock/
+
+google-chrome https://extensions.gnome.org/extension/744/hide-activities-button/
+
+google-chrome https://extensions.gnome.org/extension/600/launch-new-instance/
+
+google-chrome https://extensions.gnome.org/extension/708/panel-osd/
+
+google-chrome https://extensions.gnome.org/extension/8/places-status-indicator/
+
+google-chrome https://extensions.gnome.org/extension/53/pomodoro/
+
+google-chrome https://extensions.gnome.org/extension/905/refresh-wifi-connections/
+
+google-chrome https://extensions.gnome.org/extension/904/disconnect-wifi/
+
+google-chrome https://extensions.gnome.org/extension/7/removable-drive-menu/
+
+google-chrome https://extensions.gnome.org/extension/4506/simple-system-monitor/
+
+google-chrome https://extensions.gnome.org/extension/3891/simple-monitor/
+
+google-chrome https://extensions.gnome.org/extension/906/sound-output-device-chooser/
+
+google-chrome https://extensions.gnome.org/extension/355/status-area-horizontal-spacing/
+
+google-chrome https://extensions.gnome.org/extension/352/middle-click-to-close-in-overview/
+
+google-chrome https://extensions.gnome.org/extension/570/todotxt/
+
+google-chrome https://extensions.gnome.org/extension/1446/transparent-window-moving/
+
+google-chrome https://extensions.gnome.org/extension/1723/wintile-windows-10-window-tiling-for-gnome/
+
+google-chrome https://extensions.gnome.org/extension/427/workspaces-to-dock/
+
+
+
+
